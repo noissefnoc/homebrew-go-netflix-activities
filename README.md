cd9a23040b96409b6fcced3c61d0d0a0273abcc4
Kota SAITO <noissefnoc@gmail.com>
2018-02-26 19:10:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+homebrew-go-netflix-activities
+===============================
+
+A [Homebrew](https://brew.sh/) formula for [noissefnoc/go-netflix-activities](https://github.com/noissefnoc/go-netflix-activities)
+
---
+++ go-netflix-activities.rb
@@ -0,0 +1,10 @@
+ class GoNetflixActivities < Formula
+  desc "Fetch Netflix user's own activities."
+  homepage ""
+  url "https://github.com/noissefnoc/go-netflix-activities/releases/download/v0.0.1/netflix-activities-v0.0.1-darwin-amd64.zip"
+  sha256 "d72970ed2e0feac6a876884eae1e6bf169344c14aa4c72992b8332ee5b794e56"
+  depends_on "chromedriver"
+  def install
+    bin.install 'netflix-activities'
+  end
+end
