65ac4d66f9949b17728346c69caa2140c1364efc
noissefnoc <noissefnoc@gmail.com>
2018-02-27 15:05:11 +0900

    release for v0.0.3

--- go-netflix-activities.rb
+++ go-netflix-activities.rb
@@ -1,8 +1,8 @@
  class GoNetflixActivities < Formula
   desc "Fetch Netflix user's own activities."
   homepage "https://github.com/noissefnoc/go-netflix-activities"
-  url "https://github.com/noissefnoc/go-netflix-activities/releases/download/v0.0.2/netflix-activities-v0.0.2-darwin-amd64.zip"
-  sha256 "37595168446c85911bffb5f98d82e17e944ebb223657068c5c48c4a4c3b056b3"
+  url "https://github.com/noissefnoc/go-netflix-activities/releases/download/v0.0.3/netflix-activities-v0.0.3-darwin-amd64.zip"
+  sha256 "adf067a751c2363b448153439337d438ee1522ea112b6c7a10f71f68eedb1ee1"
   depends_on "chromedriver"
   def install
     bin.install 'netflix-activities'
