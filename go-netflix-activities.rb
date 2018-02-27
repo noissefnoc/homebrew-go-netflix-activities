55947a5cee23a2a952a66b04dc432457200a0db5
Kota SAITO <noissefnoc@gmail.com>
2018-02-27 13:22:48 +0900

    fix wrong sha256 value

--- go-netflix-activities.rb
+++ go-netflix-activities.rb
@@ -2,7 +2,7 @@
   desc "Fetch Netflix user's own activities."
   homepage "https://github.com/noissefnoc/go-netflix-activities"
   url "https://github.com/noissefnoc/go-netflix-activities/releases/download/v0.0.2/netflix-activities-v0.0.2-darwin-amd64.zip"
-  sha256 "d72970ed2e0feac6a876884eae1e6bf169344c14aa4c72992b8332ee5b794e56"
+  sha256 "37595168446c85911bffb5f98d82e17e944ebb223657068c5c48c4a4c3b056b3"
   depends_on "chromedriver"
   def install
     bin.install 'netflix-activities'
