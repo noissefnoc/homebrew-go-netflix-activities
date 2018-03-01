4ddd2249282db0a0a44008c723820a5cf9049bf9
noissefnoc <noissefnoc@gmail.com>
2018-03-02 00:29:41 +0900

    forgot file url version up

--- go-netflix-activities.rb
+++ go-netflix-activities.rb
@@ -2,7 +2,7 @@
   desc "Fetch Netflix user's own activities."
   homepage "https://github.com/noissefnoc/go-netflix-activities"
   version "v0.0.5"
-  url "https://github.com/noissefnoc/go-netflix-activities/releases/download/v0.0.4/netflix-activities-v0.0.4-darwin-amd64.zip"
+  url "https://github.com/noissefnoc/go-netflix-activities/releases/download/v0.0.5/netflix-activities-v0.0.5-darwin-amd64.zip"
   sha256 "92f981cb1f3c0e7b0d27bb0999656927b6b006e3a60fdc30bbea066449c66063"
   depends_on "chromedriver"
   def install
